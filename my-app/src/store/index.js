5a09571c6c121f0ee90a13ca20896df7ac9ce603
SergeyChudinov <sergeychudinov1986@gmail.com>
2022-09-03 17:18:31 +0500

    Make lesson-8-13

--- my-app/src/store/index.js
+++ my-app/src/store/index.js
@@ -1,16 +1,35 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
+const enhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+    const oldDispatch = store.dispatch;
+    store.dispatch = (action) => {
+        if (typeof action === 'string') {
+            return oldDispatch({
+                type: action
+            })
+        }
+        return oldDispatch(action)
+    }
+    return store
+}
 
 const reducer = combineReducers({
     heroes: heroes,
     filters: filters
 })
 
-const store = createStore(reducer, composeWithDevTools());
+const store = createStore(reducer,
+              compose(
+                enhancer,
+                composeWithDevTools()
+              ));
 
 export default store;
+
+// composeWithDevTools()
